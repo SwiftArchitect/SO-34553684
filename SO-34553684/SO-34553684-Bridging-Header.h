3784a37cffb14fe142a3253f3bd879ce9dce96e9
Xavier Schott <xschott@gmail.com>
2018-07-20 14:37:39 -0700

    Xcode 9.4.1 iOS 11.4

--- SO-34553684/SO-34553684-Bridging-Header.h
+++ SO-34553684/SO-34553684-Bridging-Header.h
@@ -1,7 +1,7 @@
 //
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
-//  Copyright © 2017 Xavier Schott
+//  Copyright © 2017, 2018 Xavier Schott
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
 //  in the Software without restriction, including without limitation the rights
